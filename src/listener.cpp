62a072599ab8ecfec36c3ec9e271613d54e351dc
aaceves <aaceves@users.noreply.github.com>
2019-05-30 21:29:50 -0500

    Comandos básicos del listener

--- src/listener.cpp
+++ src/listener.cpp
@@ -1,1 +1,17 @@
- 
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+void chatterCallback(const std_msgs::String::ConstPtr& msg) 
+{
+  std::cout << "Escuché: " << msg->data.c_str() << std::endl;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener");
+  ros::NodeHandle n;
+  ros::Subscriber chatter_sub = 
+                n.subscribe("chatter", 100, chatterCallback);
+  ros::spin();
+  return 0;
+}
